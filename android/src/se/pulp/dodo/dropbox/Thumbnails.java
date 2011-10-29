002a1e5555409cdd3a770897588f22e277dcd972
Johan Lindberg <johan@pulp.se>
2011-10-29 23:12:25 +0200

    Still not working as it should.
    I can't seem to get the cache storage working. I honestly believe that the
    download and the dropbox integration is working as it should (more or less)
    and that the problem is placing the thumb on /sdcard or in some cache dir.

--- android/src/se/pulp/dodo/dropbox/Thumbnails.java
+++ android/src/se/pulp/dodo/dropbox/Thumbnails.java
@@ -95,61 +95,48 @@ public class Thumbnails extends AsyncTask<Void, Long, Boolean> {
                     // Add it to the list of thumbs we can choose from
                     thumbs.add(ent);
 
-                    String cachePath = context.getCacheDir().getAbsolutePath() + ent.path;
+                    String cachePath = context.getDir("Thumbs", Context.MODE_PRIVATE).getAbsolutePath() + ent.path;
                     try {
-                    	fos = context.openFileOutput(cachePath, context.MODE_PRIVATE);
-                    	//fos = new FileOutputStream(cachePath);
+                    	fos = new FileOutputStream(cachePath);
+    		            dropbox.getThumbnail(ent.path, fos, ThumbSize.BESTFIT_480x320, ThumbFormat.JPEG, null);
+						fos.close();
                     } catch (FileNotFoundException e) {
                     	err = "Couldn't create a local file to store the image";
                     	return false;
-                    }
-
-		            // This downloads a smaller, thumbnail version of the file.  The
-		            // API to download the actual file is roughly the same.
-		            dropbox.getThumbnail(path, fos, ThumbSize.BESTFIT_480x320, ThumbFormat.JPEG, null);
+					} catch (IOException e) {
+                    	err = "Couldn't create a local file to store the image";
+                    	return false;
+					}
 		            
 		            if (canceled) {
 		                return false;
 		            }
-		
-		            drawables.add(Drawable.createFromPath(cachePath));
+		            
+		            Drawable drawable = Drawable.createFromPath(cachePath);
+		            drawables.add(drawable);
                 }
             }
             
             numberOfThumbnails = new Long(thumbs.size());
-            return true;
 
         } catch (DropboxUnlinkedException e) {
-            // The AuthSession wasn't properly authenticated or user unlinked.
         	err = "Idiot!";
         } catch (DropboxPartialFileException e) {
-            // We canceled the operation
             err = "Download canceled";
         } catch (DropboxServerException e) {
-            // Server-side exception.  These are examples of what could happen,
-            // but we don't do anything special with them here.
             if (e.error == DropboxServerException._304_NOT_MODIFIED) {
-                // won't happen since we don't pass in revision with metadata
             	err = "Not modified";
             } else if (e.error == DropboxServerException._401_UNAUTHORIZED) {
-                // Unauthorized, so we should unlink them.  You may want to
-                // automatically log the user out in this case.
             	err = "Unauthorized";
             } else if (e.error == DropboxServerException._403_FORBIDDEN) {
-                // Not allowed to access this
             	err = "Forbidden";
             } else if (e.error == DropboxServerException._404_NOT_FOUND) {
-                // path not found (or if it was the thumbnail, can't be
-                // thumbnailed)
             	err = "Not found";
             } else if (e.error == DropboxServerException._406_NOT_ACCEPTABLE) {
-                // too many entries to return
             	err = "Not acceptable";
             } else if (e.error == DropboxServerException._415_UNSUPPORTED_MEDIA) {
-                // can't be thumbnailed
             	err = "Unsupported media";
             } else if (e.error == DropboxServerException._507_INSUFFICIENT_STORAGE) {
-                // user is over quota
             	err = "Insufficient storage";
             } else {
                 // Something else
@@ -162,16 +149,16 @@ public class Thumbnails extends AsyncTask<Void, Long, Boolean> {
             }
         } catch (DropboxIOException e) {
             // Happens all the time, probably want to retry automatically.
-            err = "Network error.  Try again.";
+            err = "Network error. Try again.";
         } catch (DropboxParseException e) {
             // Probably due to Dropbox server restarting, should retry
-            err = "Dropbox error.  Try again.";
+            err = "Dropbox error. Try again.";
         } catch (DropboxException e) {
             // Unknown error
-            err = "Unknown error.  Try again.";
+            err = "Unknown error. Try again.";
         }
         
-        return false;
+        return err == null;
     }
 
     @Override
